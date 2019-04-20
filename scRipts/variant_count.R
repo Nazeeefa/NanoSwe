3c54b6d4322178c58a90f7b49756344ea351b608
Nazeefa <fatima.nazeefa@gmail.com>
2019-04-20 18:56:00 +0200

    Update variant_count.R

--- scRipts/variant_count.R
+++ scRipts/variant_count.R
@@ -6,9 +6,9 @@ vcf1 <- read.table("/full/path/file.csv", sep=";", header = TRUE)
 vcf1$CHROM <- factor(vcf1$CHROM, levels = paste("chr",c(seq(1,22), "X","Y"), sep = ""))
 
 library(ggplot2)
-count_plot <- ggplot() + geom_bar(aes(x = vcf1$CHROM, fill = SV), data=vcf1, position = "stack", stat="count") +
+count_plot <- ggplot() + geom_bar(aes(x = vcf1$CHROM, fill = SV_Type), data=vcf1, position = "stack", stat="count") +
   theme_minimal() + 
-  labs(title = "Counts of variants per chromosome", y = "Amount of Variants", x = "Chromosomes") + 
+  labs(title = "Counts of variants per chromosome", y = "Count", x = "Chromosomes") + 
   scale_x_discrete(labels = c(seq(1,22), "X","Y")) + 
   facet_wrap(~Sample,  scales='free') + 
   theme(
